880b77a3c717e3bde98a2ed751b8513868108c82
bhavin1711 <garg.bhavin17@gmail.com>
2026-05-20 04:09:55 +1000

    Added analytics unit tests

---
+++ backend/test_analytics.py
@@ -0,0 +1,19 @@
+from analytics import sma, compute_signal
+
+
+def test_sma_calculation():
+    prices = [1, 2, 3, 4, 5]
+
+    result = sma(prices, 5)
+
+    assert result == 3
+
+
+def test_buy_signal():
+    signal = compute_signal(
+        price=110,
+        sma20=100,
+        sma50=90
+    )
+
+    assert signal == "BUY"
